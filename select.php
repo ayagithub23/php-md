136bbbb375561cb8455041c5c3dc34a95d11c01c
celiagithub23 <neptune32ac@gmail.com>
2014-11-24 17:25:11 +0800

    Revert "select"
    This reverts commit 0a703e4f1d51e6fb7c3c7ed94cd137a362673879.

--- select.php
+++
@@ -1,8 +0,0 @@
-<?php
-// SELECT "欄位名" FROM "表格名";
-"SELECT * FROM ex2 WHERE t_name = '{$_GET["t_name"]}'";
-"SELECT t_name FROM ex4_subject WHERE t_index = '{$_GET["s_index"]}'";
-"SELECT t_index FROM ex1 WHERE t_name = '李大雲'";
-//"SELECT t_name FROM ex1 t_index  = 1"
-"SELECT t_name FROM ex1 LIMIT 0,25";
-?>
